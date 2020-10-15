1c9256fcad276d1cfb92191f620c334d04da0b9d
TabataGonzalez <tgonzalez@asgbpm.com>
2020-10-15 09:55:58 -0500

    Sección 8

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes(id,nombre,apellido,email,create_at)VALUES(1,'Andres','Guzman','andres@gmail.com','2017-08-28'); 
+INSERT INTO clientes(id,nombre,apellido,email,create_at)VALUES(2,'John','Doe','john@gmail.com','2017-08-28'); 
